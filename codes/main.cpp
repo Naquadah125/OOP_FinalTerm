05a445258afeb984e5d9397eafd984e81f0fa989
Naquadah125 <nhocktete01@gmail.com>
2025-07-17 16:11:30 +0700

    main

---
+++ codes/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "PhuongTien.cpp"
+#include "XeOto.cpp"
+#include "XeMay.cpp"
+#include "XeTai.cpp"
+using namespace std;
+
+int main() {
+    cout << "ccjday" << endl;
+    return 0;
+}
